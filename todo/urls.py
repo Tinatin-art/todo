a1be5eedc144372199f88d6b4ca5bf956948a499
Tinatin-art <omurovatinatin@gmail.com>
2021-01-09 19:49:15 +0600

    set static and media urls and roots

--- todo/urls.py
+++ todo/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 from main.views import homepage, test, second, third
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
