13bff2696ba5f0f5c6c4b4f4ca5e771d170ecad0
Tinatin-art <omurovatinatin@gmail.com>
2021-01-17 22:02:36 +0600

    book

--- main/models.py
+++ main/models.py
@@ -6,3 +6,10 @@ class ToDo(models.Model):
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
 
+
+class BookShop(models.Model):
+    text = models.CharField(max_length=100)
+    created_at = models.DateField(auto_now_add=True)
+    is_closed = models.BooleanField(default=False)
+    is_favorite = models.BooleanField(default=False)
+
--- main/views.py
+++ main/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, HttpResponse
 from .models import ToDo
+from .models import BookShop
 
 
 def homepage(request):
@@ -8,6 +9,10 @@ def homepage(request):
 def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
+
+def books(request):
+    books_list = BookShop.objects.all()
+    return render(request, "books.html", {"books_list": books_list})
 
 def second(request):
     return HttpResponse("Test 2 page")
