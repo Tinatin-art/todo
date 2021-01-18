b87badbdbe362d5c696d8915add788cb93153fb5
Tinatin-art <omurovatinatin@gmail.com>
2021-01-18 20:41:29 +0600

    models

--- main/models.py
+++ main/models.py
@@ -8,8 +8,11 @@ class ToDo(models.Model):
 
 
 class BookShop(models.Model):
-    text = models.CharField(max_length=100)
-    created_at = models.DateField(auto_now_add=True)
-    is_closed = models.BooleanField(default=False)
-    is_favorite = models.BooleanField(default=False)
-
+    title = models.CharField("Заголовок", max_length=100)
+    subtitle = models.CharField("Подзаголовок", max_length=100)
+    description = models.TextField("Описание")
+    price = models.PositiveIntegerField("Цена")
+    genres = models.CharField("Жанры", max_length=100)
+    author = models.CharField("Имя", max_length=100)
+    year = models.PositiveSmallIntegerField("Дата выхода", default=2020)
+    date = models.DateField("Дата", auto_now_add=True)
