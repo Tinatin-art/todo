d2452effe6f0c092009bf400fd3c94c9075e2609
Tinatin-art <omurovatinatin@gmail.com>
2021-01-22 14:02:40 +0600

    delete model BookShop

--- main/models.py
+++ main/models.py
@@ -5,12 +5,3 @@ class ToDo(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     is_closed = models.BooleanField(default=False)
     is_favorite = models.BooleanField(default=False)
-
-
-class BookShop(models.Model):
-    text = models.CharField(max_length=140)
-    subtitle = models.CharField(max_length=140)
-    description = models.CharField(max_length=140)
-    genres = models.CharField(max_length=140)
-    year = models.DateField(default=0)
-    
--- main/views.py
+++ main/views.py
@@ -1,6 +1,5 @@
 from django.shortcuts import render, HttpResponse, redirect
 from .models import ToDo
-from .models import BookShop
 
 
 def homepage(request):
@@ -9,10 +8,6 @@ def homepage(request):
 def test(request):
     todo_list = ToDo.objects.all()
     return render(request, "test.html", {"todo_list": todo_list})
-
-def books(request):
-    books_list = BookShop.objects.all()
-    return render(request, "books.html", {"books_list": books_list})
 
 def second(request):
     return HttpResponse("Test 2 page")
